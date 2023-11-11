7035b422c460e65260b451f6522b3c9696fcdaaa
Camille Vauchel <camille.vauchel@gmail.com>
2023-11-11 21:22:28 +0100

    Add return true false

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -116,6 +116,38 @@ func TestNextTokenWithGibberish(t *testing.T) {
 		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+
+		{token.RBRACE, "}"},
+
+		// {token.INT, "10"},
+		// {token.EQ, "=="},
+		// {token.INT, "10"},
+		// {token.SEMICOLON, ";"},
+		//
+		// {token.INT, "5"},
+		// {token.NOT_EQ, "!="},
+		// {token.INT, "10"},
+		// {token.SEMICOLON, ";"},
 	}
 
 	testLexer(lexer, tests, t)
